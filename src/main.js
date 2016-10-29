ff649f61ea2f21dffba9817662c86f94b0532d53
Will Madden <w.a.madden@gmail.com>
2016-10-29 18:13:55 +0200

    Add CSS processing

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log("hi");
+/* globals document, requestAnimationFrame */
+import * as PIXI from "pixi.js";
+import "css/main";
+
+const renderer = new PIXI.WebGLRenderer(800, 600);
+
+const stage = new PIXI.Container();
+
+// The renderer will create a canvas element for you that you can then insert into the DOM.
+document.body.appendChild(renderer.view);
+
+function animate() {
+  // start the timer for the next animation loop
+  requestAnimationFrame(animate);
+
+  // this is the main render call that makes pixi draw your container and its children.
+  renderer.render(stage);
+}
+
+requestAnimationFrame(animate);
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,45 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var SRC_DIR = process.env.npm_package_config_srcDir;
+
+require("dotenv").load({ silent: true });
+
+function absolutePathTo(relativePath) {
+  return path.join(__dirname, relativePath);
+}
 
 module.exports = {
-  entry: ['babel-polyfill', './src/main'],
+  entry: {
+    "main.js": ["babel-polyfill", "./src/main"],
+  },
 
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: "babel",
         query: {
           cacheDirectory: true,
-          presets: ['es2015'],
+          presets: ["es2015"],
         },
       },
-    ]
+      {
+        test: /\.(css|scss)$/,
+        loader: "style-loader!css-loader?sourceMaps!postcss-loader?sourceMaps",
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ["", ".js", ".css", ".mp3", ".ogg", ".flac"],
+    root: [
+      absolutePathTo(SRC_DIR),
+    ],
   },
 
   plugins: [new HtmlWebpackPlugin({
     title: "Roguelike 4",
   })],
 
-}
+};
